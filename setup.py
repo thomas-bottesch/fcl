f39a3aa0742d1b2964b2516d6f2a7290cdff6335
Thomas Bottesch <thomas.social@googlemail.com>
2025-08-06 10:37:57 +0200

    added more supported python versions

--- setup.py
+++ setup.py
@@ -164,6 +164,12 @@ setup(
                'Programming Language :: Python :: 3',
                'Programming Language :: Python :: 3.4',
                'Programming Language :: Python :: 3.5',
-               'Programming Language :: Python :: 3.6'],
+               'Programming Language :: Python :: 3.6',
+               'Programming Language :: Python :: 3.7',
+               'Programming Language :: Python :: 3.8',
+               'Programming Language :: Python :: 3.9',
+               'Programming Language :: Python :: 3.10',
+               'Programming Language :: Python :: 3.11',
+               'Programming Language :: Python :: 3.12'],
   cmdclass={'clean': CleanClass},
 )
