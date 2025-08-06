f35080755d2a802e506fa7226e0aa55adb666069
Thomas Bottesch <thomas.bottesch@gendigital.com>
2025-08-06 13:14:08 +0200

    removed OCTAVE from tests

--- tools_build/ci/run.sh
+++ tools_build/ci/run.sh
@@ -4,23 +4,6 @@
 set -e                         # Exit immediately if a command exits with a non-zero status. 
 set -x                         # Print commands and their arguments as they are executed.
 
-if [ "x$INSTALL_OCTAVE" = "xyes" ]; then
-    # make sure the octave examples work
-    # mex files are compiled automatically when executing the first example
-    for BASE_FOLDER in examples/matlab/kmeans/basic/ examples/matlab/kmeans/plotting/
-    do
-        FILES=${BASE_FOLDER}*.m
-        for f in $FILES
-        do
-          echo octave -p $BASE_FOLDER --eval `basename "${f%.*}"`
-          echo
-          
-          octave -p $BASE_FOLDER --eval `basename "${f%.*}"`
-          echo "------------------------------------------------"
-        done
-    done
-fi
-
 if [ "x$INSTALL_PYTHON_REQUIREMENTS" = "xyes" ]; then
     python setup.py install
     
